ae93d027bdf39a2f425bbe1cbc051cb21e2476bb
jongkyuh <jongkyuh@naver.com>
2023-05-13 23:31:23 +0900

    [말] 말의 기본적인 특성을 담은 슈퍼클래스
    말의 기본적인 특성을 담은 슈퍼클래스와 두개의 말 클래스2개ㅑ 선언해서 메인해서 구동해봤습니다. 메인에 제가 선언한거 머지할 때 머지 하지 않으면 될거같습니다.

--- src/main/java/uma/sahmyook/Application.java
+++ src/main/java/uma/sahmyook/Application.java
@@ -1,4 +1,25 @@
 package uma.sahmyook;
-
+import uma.sahmyook.horse.*;
 public class Application {
+
+    public static void main(String[] args) {
+
+        /* 체크해보려고 만들었던 거입니다 머지할때 올리지마세용!!! 중요!!
+
+//        Horse1 horse1 = new Horse1();
+//        Horse2 horse2 = new Horse2();
+
+        //Horse1 스테미너 다시 100으로 되는지 체크
+
+        for(int i = 0;i<7;i++){
+            System.out.println("horse1이 달리는 속도 : " + horse1.run());
+            System.out.println("horse1의 스테미너 : " + horse1.stamina);
+            System.out.println();
+            System.out.println("horse2가 달리는 속도 : " + horse2.run());
+            System.out.println("horse2의 스테미너 : " + horse2.stamina);
+
+
+ */
+        }
+    }
 }
