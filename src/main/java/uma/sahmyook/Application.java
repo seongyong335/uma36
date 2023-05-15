e1669ea66ddbe281fee5e9c25d9f58a1e2a4731a
CHOI <seongyong335@gmail.com>
2023-05-15 15:12:43 +0900

    Merge

---
+++ src/main/java/uma/sahmyook/Application.java
@@ -0,0 +1,14 @@
+package uma.sahmyook;
+
+import uma.sahmyook.function.*;
+import uma.sahmyook.horse.*;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+            /* 경기 생성 및 시작 */
+            UmaRace ur = new UmaRace();
+            ur.startGame();
+    }
+}
