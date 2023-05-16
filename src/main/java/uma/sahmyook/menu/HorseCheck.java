11290cd9c6450d287a1af73b89ff07f8402c554d
DESKTOP-67GVLQD\seongmin <dltgld72@naver.com>
2023-05-16 20:00:55 +0900

    #4 [메뉴] 경기 메뉴 구현
    응원마 선택 메소드 추가 [미완]

---
+++ src/main/java/uma/sahmyook/menu/HorseCheck.java
@@ -0,0 +1,14 @@
+package uma.sahmyook.menu;
+
+public class HorseCheck {
+
+    public void method(){
+        System.out.println("================= 경기마 승률 및 배당률 =================");
+        for(int i = 1; i < 10; i++){
+           // System.out.println(i + "번 마의 승률 : " + ,method([i]) /* 승률 메소드 */ "%  | 배당률 : " /* 배당률 메소드 */);
+        }
+        System.out.print("Enter키를 누르면 되돌아 갑니다...");
+        MainMenu.sc.nextLine();
+        MainMenu.sc.nextLine();
+    }
+}
