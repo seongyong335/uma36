c52b75310e4e24471e1dbcc5ba06d2d578c99078
AzureAD\양한진 <gkswls3203@naver.com>
2023-05-16 17:37:05 +0900

    [#19][초안] 추후 수정 예정 응원마 변수와 1등마 동일시

--- src/main/java/uma/sahmyook/score/BettingScore.java
+++ src/main/java/uma/sahmyook/score/BettingScore.java
@@ -7,8 +7,15 @@ public class BettingScore {
     //경기 종료 후 선택한 말 초기화, 1등 우승시 스코어 증가
     //
     public static void bettingResult(){ // 선택한 말이 1등 할 시 마권 수 만큼 배당률에 계산해서 보유 스코어 + 베팅 스코어
-       // 선택말이 int rank = 1 이 맞을 경우 현재 스코어 + (ticket * 배당률)을 한다. 그 후에 useticket
-        System.out.println("베팅 결과 출력 예정");
+       // 선택말이 int rank = 1 이 맞을 경우 현재 스코어 + (ticket * 배당률)을 한다. 그 후에 roundreset
+        if (MainMenu.horseNum == 1/* 임시 if " 선택말이 1등일 경우*/){
+            System.out.println("(말 이름)"+"! 훌륭히 완승!! 레이스를 제패합니다!.");
+            MainMenu.score += (MainMenu.ticket * 1000);
+        }
+        else {
+            System.out.println("(말 이름)" +"이(가) 제패에 실패했습니다.");
+
+        }
 
 
     }
