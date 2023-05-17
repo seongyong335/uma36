56440a728b23738e6aee2db13fb850f2ad3514f8
DESKTOP-67GVLQD\seongmin <dltgld72@naver.com>
2023-05-17 19:34:25 +0900

    #4 [메뉴] 메뉴선택시 문자열 넣으면 오류 생기는 문제 해결
    int형으로 받지 않고 nextLine으로 바꿔서 해결

--- src/main/java/uma/sahmyook/menu/SubMenu.java
+++ src/main/java/uma/sahmyook/menu/SubMenu.java
@@ -14,6 +14,7 @@ public class SubMenu{
         HorseSelect horseSelect = new HorseSelect();
         HorseCheck horseCheck = new HorseCheck();
         UmaStart umaStart = new UmaStart();
+        Ticket ticket = new Ticket();
 
 
         do{
@@ -25,17 +26,16 @@ public class SubMenu{
             System.out.println("4. 마권 구입");
             System.out.println("5. 경기장 퇴장");
             System.out.print("메뉴 선택 : ");
-            MainMenu.no = MainMenu.sc.nextInt();
+            MainMenu.no = MainMenu.sc.nextLine();
+
             switch(MainMenu.no){
-                case 1: umaStart.umaStart(); break; // 경기 시작 메소드;
-                case 2: horseSelect.method(); break; // 응원하는 말 선택하는 메소드;
-                case 3: horseCheck.method(); break; // 경마 승률 및 배당률 확인 하는 메소드;
-                case 4:
-                    Ticket ticket = new Ticket();
-                    ticket.buyTicket(); break; // 마권 구입하는 메소드;
-                case 5: MainMenu.menuExit.Method(0); break;
+                case "1": umaStart.umaStart(); break; // 경기 시작 메소드;
+                case "2": horseSelect.method(); break; // 응원하는 말 선택하는 메소드;
+                case "3": horseCheck.method(); break; // 경마 승률 및 배당률 확인 하는 메소드;
+                case "4": ticket.buyTicket(); break; // 마권 구입하는 메소드;
+                case "5": MainMenu.menuExit.Method(0); break;
                 default:  System.out.println("잘못 입력하셨습니다 다시 입력해주세요!");
             }
-        } while(MainMenu.no != 5); // 이렇게 하지 않으면 case5에서 return받으면 바로 주 메뉴로 나가짐
+        } while(MainMenu.no != "5"); // 이렇게 하지 않으면 case5에서 return받으면 바로 주 메뉴로 나가짐
     }
 }
