108108682df08fe5e0c5e1fa99ceafc8d06aa3d9
GANG철\kang6 <kmc6381@gmail.com>
2023-05-18 18:13:19 +0900

    [말][PineHouse]
     체력을 80으로 설정합니다.
     속도를 현재 속도의 9배로 증가시킵니다.

---
+++ src/main/java/uma/sahmyook/horse/PineHouse.java
@@ -0,0 +1,39 @@
+package uma.sahmyook.horse;
+
+public class PineHouse extends MainHorse implements HorseInterface2 {
+
+    public PineHouse() {
+    }
+
+    public PineHouse(String name) {
+        super(name);
+    }
+
+    @Override
+    public void booster() {
+        setStamina(80);
+        setSpeed(getSpeed() * 9);
+    }
+
+    @Override
+    public int calMove() {
+        randomSpeed();
+        if (getStamina() <= 50) {
+            booster();
+        }
+        useStamina();
+
+        if (getStamina() <= 50) {
+            setSpeed(getSpeed() / 2);
+        }
+//        getStamina가 50 이상이고 getSpeed가 10 이상인 경우에 이 조건에 해당하는 말은 속도를 20 증가시킵니다.
+
+        // 체력과 속도에 따라 능력을 향상시킵니다
+        if (getStamina() >= 80 && getSpeed() >= 10) {
+            setSpeed(getSpeed() + 50);
+
+        }
+
+        return getSpeed();
+    }
+}
