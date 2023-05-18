a93a34a2e0e299e656444632f4ac23c62e317f88
jongkyuh <jongkyuh@naver.com>
2023-05-18 17:06:38 +0900

    #2 [말] 말 패키지 주석 추가

--- src/main/java/uma/sahmyook/horse/Horse4.java
+++ src/main/java/uma/sahmyook/horse/Horse4.java
@@ -2,11 +2,10 @@ package uma.sahmyook.horse;
 
 public class Horse4 extends MainHorse{
     //기본 말 기능만 가짐
-    public Horse4() {
-    }
+    public Horse4() {}                                          // 기본 생성자
 
     public Horse4(String nam) {             //경주마 생성과 해당 경주마를 대표하는 아이콘 설정
         super(nam);
-    }
+    }                   // 말의 이름을 매개변수로 받는 생성자
 
 }
--- src/main/java/uma/sahmyook/horse/Horse8.java
+++ src/main/java/uma/sahmyook/horse/Horse8.java
@@ -2,22 +2,21 @@ package uma.sahmyook.horse;
 
 public class Horse8 extends MainHorse{
 //초반엔 빠르다가 나중엔 다른 말과 같은 랜덤속도를 가지는 말
-    public Horse8() {
-    }
+    public Horse8() {}                                                        // 기본 생성자
 
     public Horse8(String nam) {             //경주마 생성과 해당 경주마를 대표하는 아이콘 설정
         super(nam);
-    }
+    }                                 // 말의 이름을 매개변수로 받는 생성자
 
     @Override
-    public int calMove() {                                   // 실행하는 클래스에서 인스턴스 생성하고 run() 메소드가 실행하면됨
+    public int calMove() {                                   // 말의 속도를 리턴받는 메소드
         randomSpeed();
         useStamina();
         if(getStamina()<100 && getStamina()>50){
             setSpeed(getSpeed()*2);
         }
 
-        if (getStamina() <= 50) {                                 //스테미너가 50이하로 떨어지면 스피드가 반으로 줄어든다.
+        if (getStamina() <= 50) {
             setSpeed(getSpeed() / 2);
         }
 
